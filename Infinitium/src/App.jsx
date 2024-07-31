0f3558f4fcb928dc9c2f32f429ae709820e26a09
GnanadeepJ <gnanadeep.jasty@gmail.com>
2024-07-31 11:28:31 -0700

    inital

---
+++ Infinitium/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>Document</title>
+        </head>
+        <body></body>
+      </html>
+    </>
+  );
+}
+
+export default App;
